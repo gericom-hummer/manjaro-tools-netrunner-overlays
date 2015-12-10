e114f0d6ff85ffaf012287815f5bf45bed6985eb
AJSlye <jameskittsmiller@gmail.com>
2015-12-10 00:12:32 -0500

    modifications needed for Manjaro-tools 0.9.15.

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 user=ubuntu
-path=var/www/ISO/
+path=var/www/ISO
 server=54.246.99.99
-files=*15.09*
-pwfile=`pwd`/passwd
+files=*next*
+pwfile=.ssh/identity/arch.pem
 sshpass -f $pwfile rsync -vP  --progress -e ssh $files $user@$server:/$path
